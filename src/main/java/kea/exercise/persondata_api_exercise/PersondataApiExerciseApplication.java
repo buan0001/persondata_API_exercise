85247fe87274a83815f288a5e9ed17705b8279e5
buan0001 <123996422+buan0001@users.noreply.github.com>
2024-05-01 10:29:35 +0200

    Initial commit

---
+++ src/main/java/kea/exercise/persondata_api_exercise/PersondataApiExerciseApplication.java
@@ -0,0 +1,13 @@
+package kea.exercise.persondata_api_exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersondataApiExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersondataApiExerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kea/exercise/persondata_api_exercise/PersondataApiExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package kea.exercise.persondata_api_exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersondataApiExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
